91f44fc31971770b93e8192e00d78943f89b208b
Bryan Parada <bryan.parada.cea@gmail.com>
2023-02-28 17:21:25 -0300

    FA147 - Creating first Cubit

---
+++ lib/bloc/user/user_cubit.dart
@@ -0,0 +1,16 @@
+
+
+// import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'user_state.dart';
+
+class UserCubit extends Cubit<UserState>{
+
+  UserCubit(): super(UserInitial() );
+
+
+
+
+}
---
+++ lib/bloc/user/user_state.dart
@@ -0,0 +1,10 @@
+part of 'user_cubit.dart';
+
+@immutable
+abstract class UserState{}
+
+class UserInitial extends UserState{
+
+  final existsUser = false;
+  
+}
